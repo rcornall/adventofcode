4547677f442c65d929a69cd00f344f4356b77104
Rob Cornall <robthecornal@gmail.com>
2020-12-14 00:21:25 -0800

    AoC: 4.2

--- 4.c
+++ 4.c
@@ -1,71 +1,92 @@
 #include <stdio.h>
 #include <string.h>
-
-struct passport {
-    char* byr;
-    char* iyr;
-    char* eyr;
-    char *hgt;
-    char *hcl;
-    char *ecl;
-    char* pid;
-    char* cid;
-};
+#include <stdlib.h>
 
 int main()
 {
     int valid = 0;
 
     while (1) {
-        struct passport p;
-
         char line[255];
-        char *tmpline;
         if (fgets(line, 255, stdin) == NULL) {
             fprintf(stderr, "Failed to fgets\n");
             return -1;
         }
 
         int fields = 0;
+        char *tmpline;
 
+        /* becoming painful in C.. */
         while (line[0] != '\n') {
-
-
             char key[4];
             char val[30] = {0};
             tmpline = line;
-            while (sscanf(tmpline, "%3[^:]:%s", key,val) == 2) {
-                /* key[3] = '\0'; */
-                /* val[3] = '\0'; */
-                printf("k,v: %s,%s\n", key, val);
+            while (sscanf(tmpline, "%3[^:]:%29s", key,val) == 2) {
                 tmpline = tmpline + strlen(key) + strlen(val) + 2;
-                printf("%lu %lu %d\n", strlen(key), strlen(val), 1);
-                printf("tmpline: %s\n", tmpline);
 
                 if (strcmp(key,"byr") == 0) {
-                    fields++;
-                    p.byr = strdup(val);
+                    char *end;
+                    unsigned long l = strtol(val, &end, 10);
+                    if (l >= 1920 && l <= 2002)
+                        fields++;
                 } else if (strcmp(key,"iyr") == 0) {
-                    fields++;
-                    p.iyr = strdup(val);
+                    char *end;
+                    unsigned long l = strtol(val, &end, 10);
+                    if (l >= 2010 && l <= 2020)
+                        fields++;
                 } else if (strcmp(key,"eyr") == 0) {
-                    fields++;
-                    p.eyr = strdup(val);
+                    char *end;
+                    unsigned long l = strtol(val, &end, 10);
+                    if (l >= 2020 && l <= 2030)
+                        fields++;
                 } else if (strcmp(key,"hgt") == 0) {
-                    fields++;
-                    p.hgt = strdup(val);
+                    char *end;
+                    unsigned long l = strtol(val, &end, 10);
+                    if (strchr(val, 'c') != NULL) {
+                        if (l >= 150 && l <= 193)
+                            fields++;
+                    } else if (strchr(val, 'i') != NULL) {
+                        if (l >= 59 && l <= 76)
+                            fields++;
+                    }
                 } else if (strcmp(key,"hcl") == 0) {
-                    fields++;
-                    p.hcl = strdup(val);
+                    if (val[0] == '#') {
+                        fields++;
+                        for (int i=1; i<strlen(val);  i++) {
+                            if ((val[i] >= 48 && val[i] <= 57) ||
+                                (val[i] >= 97 && val[i] <= 102))
+                            {
+                                continue;
+                            } else {
+                                fields--;
+                                break;
+                            }
+                        }
+                    }
                 } else if (strcmp(key,"ecl") == 0) {
-                    fields++;
-                    p.ecl = strdup(val);
+                    if (strcmp(val, "amb") == 0 ||
+                        strcmp(val, "blu") == 0 ||
+                        strcmp(val, "brn") == 0 ||
+                        strcmp(val, "gry") == 0 ||
+                        strcmp(val, "grn") == 0 ||
+                        strcmp(val, "hzl") == 0 ||
+                        strcmp(val, "oth") == 0)
+                    {
+                        fields++;
+                    }
                 } else if (strcmp(key,"pid") == 0) {
-                    fields++;
-                    p.pid = strdup(val);
+                    if (strlen(val) == 9) {
+                        fields++;
+                        for (int i=0; i<strlen(val); i++) {
+                            if ((val[i] < 48 && val[i] > 57))
+                            {
+                                fields--;
+                                break;
+                            }
+                        }
+                    }
                 } else if (strcmp(key,"cid") == 0) {
                     /* fields++; */
-                    p.cid = strdup(val);
                 }
             }
 
@@ -78,10 +99,7 @@ int main()
         if (fields >= 7)
             valid++;
 
-        printf("processed line: %s, valid: %d\n", line, valid);
-
-        /* new line */
-        printf("new line..\n");
+        printf("valid: %d\n", valid);
     }
 
     printf("valid: %d\n", valid);

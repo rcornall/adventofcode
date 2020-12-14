d761ec217074261080ad5157bafb49f65af4aaa0
Rob Cornall <robthecornal@gmail.com>
2020-12-14 01:18:56 -0800

    AoC: 6.2

--- 6.c
+++ 6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -12,16 +13,31 @@ int main()
             return -1;
         }
 
-        int unique = 0;
-        _Bool map[255] = {0};
+        /* points to arrays of maps, assume max 10 ppl per test. */
+        _Bool *maps[10] = {0};
 
+        int unique = 0; /* count unique answers */
+        int answers[255] = {0}; /* store unique answers */
+        _Bool uniques[255] = {0}; /* cache if already stored. */
+
+        /* build a map for each persons answers,
+         * then check all maps contain the answer */
+        int persons = 0;
         while (line[0] != '\n') {
+            maps[persons] = malloc(255);
+            _Bool *map = maps[persons];
+            persons++;
+
             char tmp[255];
             sscanf(line, "%s", tmp);
             for (int i=0; i<strlen(tmp); i++) {
                 if (!map[tmp[i]]) {
                     map[tmp[i]] = 1;
-                    unique++;
+
+                    if (!uniques[tmp[i]]) {
+                        uniques[tmp[i]] = 1;
+                        answers[unique++] = tmp[i];
+                    }
                 }
             }
 
@@ -31,7 +47,24 @@ int main()
             }
         }
 
-        ans = ans + unique;
+        for (int i=0; i<unique; i++) {
+            _Bool missing = 0;
+            printf("ans %d\n", answers[i]);
+            for (int j=0; j<persons; j++) {
+                printf("%d\n", maps[j][answers[i]]);
+                if (!maps[j][answers[i]]) {
+                    missing = 1;
+                    break;
+                }
+            }
+            if (!missing)
+                ans++;
+        }
+
+        /* for (int i=0; i<persons; i++) { */
+            /* free(maps[i]); */
+            /* maps[i] = NULL; */
+        /* } */
 
         printf("ans: %d\n", ans);
     }
